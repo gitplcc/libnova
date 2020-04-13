93afec48cfb7dfa2182a6740bca52af920f013d4
Pedro Luis Castedo Cepeda <pedroluis.castedo@upm.es>
2020-04-13 11:31:38 +0200

    Enhance CMake support

---
+++ config.h.in.cmake
@@ -0,0 +1,30 @@
+#cmakedefine HAVE_MALLOC_H 1
+#cmakedefine HAVE_ALLOCA_H 1
+#cmakedefine HAVE_ALLOCA 1
+
+#cmakedefine HAVE_SYS_TIME_H 1
+#cmakedefine TIME_WITH_SYS_TIME 1
+#cmakedefine TM_IN_SYS_TIME LANGUAGE C 1
+#cmakedefine HAVE_GETTIMEOFDAY 1
+#cmakedefine HAVE_TZSET 1
+
+#cmakedefine HAVE_STRING_H 1
+#cmakedefine HAVE_STRINGS_H 1
+#cmakedefine HAVE_STRCHR 1
+#cmakedefine HAVE_STRPBRK 1
+#cmakedefine HAVE_STRTOL 1
+#cmakedefine HAVE_STRTOK_R 1
+
+#cmakedefine HAVE_MODF 1
+#cmakedefine HAVE_POW 1
+#cmakedefine HAVE_SQRT 1
+#cmakedefine HAVE_CBRT 1
+#cmakedefine HAVE_ROUND 1
+#cmakedefine HAVE_COSL 1
+#cmakedefine HAVE_SINL 1
+#cmakedefine HAVE_ACOSL 1
+#cmakedefine HAVE_ASINL 1
+#cmakedefine HAVE_ATAN2L 1
+
+#cmakedefine HAVE_SYS_STAT_H 1
+#cmakedefine HAVE_DLFCN_H 1
