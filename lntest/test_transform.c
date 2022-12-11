d910b1b7869453c84099da6219f951a9e4771d26
Pedro Luis Castedo Cepeda <pedroluis.castedo@upm.es>
2022-12-12 00:58:02 +0100

    Fix & tighten transform.c tests

--- lntest/test_transform.c
+++ lntest/test_transform.c
@@ -27,7 +27,7 @@
 
 #include <unity.h>
 
-#define ARCSEC_TENTH  (0.1 / 3600.0)
+#define MARCSEC  (0.001 / 3600.0)
 
 void setUp()
 {
@@ -39,18 +39,18 @@ void tearDown()
 
 void test_get_ecl_from_equ()
 {
-  struct ln_equ_posn object = { /* Pollux coordinates (apparent) */
-    .ra  = 105.778176, /* 07h 44' 32.294" */
-    .dec =  27.972272  /* 28° 03' 27.66" */
-  }; //alt60.443811 az103.794356
-  double JD = 2446896.22292;  /* 1987/4/10 19:21:0.0 UTC */
+  /* Pollux equatorial (2000) */
+  struct ln_equ_posn object = {
+    .ra  = 116.32894167, /* 07h 45' 18.946" */
+    .dec =  28.02618333  /* 28° 01' 34.26" */
+  };
+  double JD = 2446896.30625;  /* 1987/4/10 19:21:0.0 UTC */
 
   struct ln_lnlat_posn ecl;
   ln_get_ecl_from_equ(&object, JD, &ecl);
 
-  printf("L =\t% 3.12f\nB =\t% 3.12f\n", ecl.lng, ecl.lat);
-  TEST_ASSERT_DOUBLE_WITHIN(ARCSEC_TENTH, 113.526097, ecl.lng);
-  TEST_ASSERT_DOUBLE_WITHIN(ARCSEC_TENTH,   6.687989, ecl.lat);
+  TEST_ASSERT_DOUBLE_WITHIN(MARCSEC, 113.21555278, ecl.lng);
+  TEST_ASSERT_DOUBLE_WITHIN(MARCSEC,   6.68264899, ecl.lat);
 }
 
 void test_get_ecl_from_rect()
@@ -65,14 +65,14 @@ void test_get_equ2000_from_gal()
 
 void test_get_equ_from_ecl()
 {
-  struct ln_lnlat_posn ecl = {.lng = 113.039166667, .lat = 6.68388888889};
-  double JD = 2446896.22292;  /* 1987/4/10 19:21:0.0 UTC */
+  struct ln_lnlat_posn ecl = {.lng =  113.21555278, .lat = 6.68264899};
+  double JD = 2446896.30625;  /* 1987/4/10 19:21:0.0 UTC */
 
   struct ln_equ_posn equ;
   ln_get_equ_from_ecl(&ecl, JD, &equ);
 
-  TEST_ASSERT_DOUBLE_WITHIN(ARCSEC_TENTH, 116.32894167, equ.ra);
-  TEST_ASSERT_DOUBLE_WITHIN(ARCSEC_TENTH,  28.02618333, equ.dec);
+  TEST_ASSERT_DOUBLE_WITHIN(MARCSEC, 116.32894167, equ.ra);
+  TEST_ASSERT_DOUBLE_WITHIN(MARCSEC,  28.02618333, equ.dec);
 }
 
 void test_get_equ_from_gal()
@@ -83,21 +83,39 @@ void test_get_equ_from_gal()
   struct ln_equ_posn equ;
   ln_get_equ_from_gal(&gal, &equ);
 
-  TEST_ASSERT_DOUBLE_WITHIN(ARCSEC_TENTH, 192.25, equ.ra);
-  TEST_ASSERT_DOUBLE_WITHIN(ARCSEC_TENTH,  27.40, equ.dec);
+  TEST_ASSERT_DOUBLE_WITHIN(MARCSEC, 192.25, equ.ra);
+  TEST_ASSERT_DOUBLE_WITHIN(MARCSEC,  27.40, equ.dec);
 }
 
 void test_get_equ_from_hrz()
 {
-  struct ln_hrz_posn object_hrz = {.az = 65.015, .alt = 14.0167777778};
-  struct ln_lnlat_posn observer = {.lng = 282.934444444, .lat = 38.9213888889};
-  double JD = 2446896.22292;  /* 1987/4/10 19:21:0.0 UTC */
+  /* Object position */
+  struct ln_equ_posn object = {
+    .ra  = 347.3193375,  /* 23h 09' 16.641" */
+    .dec =  -6.71989166  /* -6° 43' 11.61" */
+
+  };
+  /* Observer site */
+  struct ln_lnlat_posn observer = {
+    .lng = 282.934444444,  /* 282° 56'  4.0" */
+    .lat = 38.9213888889   /*  38° 55' 17.0" */
+  };
+
+  double JD = 2446896.30625;  /* Observer date: 1987/4/10 19:21:0.0 UTC */
+
+  struct ln_hrz_posn hrz;
+  ln_get_hrz_from_equ_sidereal_time(
+    &object,
+    &observer,
+    ln_get_apparent_sidereal_time(JD),
+    &hrz
+  );
 
   struct ln_equ_posn object_equ;
-  ln_get_equ_from_hrz(&object_hrz, &observer, JD, &object_equ);
+  ln_get_equ_from_hrz(&hrz, &observer, JD, &object_equ);
 
-  TEST_ASSERT_DOUBLE_WITHIN(ARCSEC_TENTH, 192.25, object_equ.ra);
-  TEST_ASSERT_DOUBLE_WITHIN(ARCSEC_TENTH,  27.40, object_equ.dec);
+  TEST_ASSERT_DOUBLE_WITHIN(MARCSEC, object.ra, object_equ.ra);
+  TEST_ASSERT_DOUBLE_WITHIN(MARCSEC, object.dec, object_equ.dec);
 }
 
 void test_get_gal_from_equ()
@@ -108,7 +126,7 @@ void test_get_gal_from_equ()
   struct ln_gal_posn gal;
   ln_get_gal_from_equ(&equ, &gal);
 
-  TEST_ASSERT_DOUBLE_WITHIN(ARCSEC_TENTH, 90.0, gal.b);
+  TEST_ASSERT_DOUBLE_WITHIN(MARCSEC, 90.0, gal.b);
 }
 
 void test_get_gal_from_equ2000()
@@ -125,9 +143,10 @@ void test_get_gal_from_equ2000()
 
 void test_get_hrz_from_equ()
 {
-  struct ln_equ_posn object = { /* Pollux coordinates (apparent) */
-    .ra  = 105.742299722,  /* 07h 44' 32.294" */
-    .dec =  28.0608194444  /* 28° 03' 27.66" */
+  /* Object position */
+  struct ln_equ_posn object = {
+    .ra  = 347.3193375,  /* 23h 09' 16.641" */
+    .dec =  -6.71989166  /* -6° 43' 11.61" */
 
   };
   /* Observer site */
@@ -135,36 +154,13 @@ void test_get_hrz_from_equ()
     .lng = 282.934444444,  /* 282° 56'  4.0" */
     .lat = 38.9213888889   /*  38° 55' 17.0" */
   };
-  double JD = 2446896.22292;  /* 1987/4/10 19:21:0.0 UTC */
+  double JD = 2446896.30625;  /* 1987/4/10 19:21:0.0 UTC */
 
   struct ln_hrz_posn hrz;
   ln_get_hrz_from_equ(&object, &observer, JD, &hrz);
 
-  TEST_ASSERT_DOUBLE_WITHIN(ARCSEC_TENTH, 15.12426274, hrz.alt);
-  TEST_ASSERT_DOUBLE_WITHIN(ARCSEC_TENTH, 68.03429264, hrz.az);
-}
-
-void test_get_hrz_from_equ_sidereal_time()
-{
-  struct ln_equ_posn object = { /* Pollux coordinates (apparent) */
-    .ra  = 105.742299722,  /* 07h 44' 32.294" */
-    .dec =  28.0608194444  /* 28° 03' 27.66" */
-
-  };
-
-  /* Observer site */
-  struct ln_lnlat_posn observer = {
-    .lng = 282.934444444,  /* 282° 56'  4.0" */
-    .lat =  38.9213888889  /*  38° 55' 17.0" */
-  };
-  /* Observer local sidereal time */
-  double lst = 1.8725;     /* 01h 26m 21s */
-
-  struct ln_hrz_posn hrz;
-  ln_get_hrz_from_equ_sidereal_time(&object, &observer, lst, &hrz);
-
-  TEST_ASSERT_DOUBLE_WITHIN(ARCSEC_TENTH, 15.12426274, hrz.alt);
-  TEST_ASSERT_DOUBLE_WITHIN(ARCSEC_TENTH, 68.03429264, hrz.az);
+  TEST_ASSERT_DOUBLE_WITHIN(MARCSEC, 15.12426274, hrz.alt);
+  TEST_ASSERT_DOUBLE_WITHIN(MARCSEC, 68.03429264, hrz.az);
 }
 
 void test_get_rect_from_helio()
@@ -172,22 +168,9 @@ void test_get_rect_from_helio()
   TEST_IGNORE();
 }
 
-void test_get_nutation()
-{
-  double JD = 2446896.22292;  /* 1987/4/10 19:21:0.0 UTC */
-
-  struct ln_nutation nutation;
-  ln_get_nutation(JD, &nutation);
-
-  TEST_ASSERT_EQUAL_DOUBLE( 0.002629211838, nutation.obliquity);
-  TEST_ASSERT_EQUAL_DOUBLE(23.4409460039, nutation.ecliptic);
-}
-
 int main(int argc, char **argv)
 {
   UNITY_BEGIN();
-
-  RUN_TEST(test_get_nutation);
 
   RUN_TEST(test_get_ecl_from_equ);
   RUN_TEST(test_get_ecl_from_rect);
@@ -198,7 +181,6 @@ int main(int argc, char **argv)
   RUN_TEST(test_get_gal_from_equ);
   RUN_TEST(test_get_gal_from_equ2000);
   RUN_TEST(test_get_hrz_from_equ);
-  RUN_TEST(test_get_hrz_from_equ_sidereal_time);
   RUN_TEST(test_get_rect_from_helio);
 
   return UNITY_END();
